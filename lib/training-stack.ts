df7b07dc1f25b37638f03e248bcb72ce814eba84
Le Tuan Anh <anhlt2@vnext.com.vn>
2021-11-29 14:45:08 +0700

    Update style

--- lib/training-stack.ts
+++ lib/training-stack.ts
@@ -18,19 +18,21 @@ import * as cdk from "@aws-cdk/core";
 import { Duration } from "@aws-cdk/core";
 
 export class TrainingStack extends cdk.Stack {
-    public vpc: Vpc;
-
     constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
 
-        // Create VPC
-        this.vpc = new Vpc(this, "vpc", {
+        /**
+         * Create VPC
+         */
+        const vpc = new Vpc(this, "vpc", {
             cidr: "13.0.0.0/16",
             maxAzs: 2,
             natGateways: 1,
             subnetConfiguration: [
                 {
-                    subnetType: SubnetType.PRIVATE_WITH_NAT,
+                    subnetType: process.env.DEBUG
+                        ? SubnetType.PUBLIC
+                        : SubnetType.PRIVATE_WITH_NAT,
                     name: "private-subnet",
                     cidrMask: 24,
                 },
@@ -42,29 +44,37 @@ export class TrainingStack extends cdk.Stack {
             ],
         });
 
-        // Create Security Group
+        /**
+         * Create Security Group
+         */
+        // Bastion SG
         const bastionSg = new SecurityGroup(this, "bastion-sg", {
-            vpc: this.vpc,
+            vpc,
             allowAllOutbound: true,
         });
         bastionSg.addIngressRule(Peer.anyIpv4(), Port.tcp(22));
 
-        const elbSg = new SecurityGroup(this, "elb-sg", {
-            vpc: this.vpc,
+        // ALB SG
+        const albSg = new SecurityGroup(this, "elb-sg", {
+            vpc,
             allowAllOutbound: true,
         });
-        elbSg.addIngressRule(Peer.anyIpv4(), Port.tcp(80));
+        albSg.addIngressRule(Peer.anyIpv4(), Port.tcp(80));
 
+        // Webserver SG
         const webserverSg = new SecurityGroup(this, "webserver-sg", {
-            vpc: this.vpc,
+            vpc,
             allowAllOutbound: true,
         });
         webserverSg.connections.allowFrom(bastionSg, Port.tcp(22));
-        webserverSg.connections.allowFrom(elbSg, Port.tcp(80));
+        webserverSg.connections.allowFrom(albSg, Port.tcp(80));
 
-        // Create EC2 Instance
+        /**
+         * Create EC2 Instance
+         */
+        // Bastion Instance
         new Instance(this, "bastion", {
-            vpc: this.vpc,
+            vpc,
             vpcSubnets: {
                 subnetType: SubnetType.PUBLIC,
             },
@@ -76,6 +86,7 @@ export class TrainingStack extends cdk.Stack {
             keyName: "ec2-key-pair",
         });
 
+        // Webserver Instance
         const userData = UserData.forLinux();
         userData.addCommands(
             "sudo -i",
@@ -86,9 +97,11 @@ export class TrainingStack extends cdk.Stack {
         );
 
         const webserverEc2 = new Instance(this, "webserver", {
-            vpc: this.vpc,
+            vpc,
             vpcSubnets: {
-                subnetType: SubnetType.PRIVATE_WITH_NAT,
+                subnetType: process.env.DEBUG
+                    ? SubnetType.PUBLIC
+                    : SubnetType.PRIVATE_WITH_NAT,
             },
             instanceType: InstanceType.of(InstanceClass.T2, InstanceSize.MICRO),
             machineImage: new AmazonLinuxImage({
@@ -99,18 +112,22 @@ export class TrainingStack extends cdk.Stack {
             userData,
         });
 
-        // Create Application Load Balancer
+        /**
+         * Create Application Load Balancer
+         */
         const alb = new ApplicationLoadBalancer(this, "alb", {
-            vpc: this.vpc,
+            vpc,
             internetFacing: true,
-            securityGroup: elbSg,
+            securityGroup: albSg,
             vpcSubnets: {
                 subnetType: SubnetType.PUBLIC,
             },
         });
+
         const listener = alb.addListener("alb-listener", {
             port: 80,
         });
+
         listener.addTargets("alb-listener-target", {
             port: 80,
             targets: [new InstanceIdTarget(webserverEc2.instanceId)],
